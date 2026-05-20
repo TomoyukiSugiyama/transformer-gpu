175c2d11e4b7c5d46146ffd1860ce0558d2662fa
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2026-05-20 23:33:35 +0900

    QK^T / √d_k までのシェーダー追加

--- src/kernel/attention.rs
+++ src/kernel/attention.rs
@@ -1,4 +1,115 @@
+use wgpu::util::DeviceExt;
 
+const TILE: u32 = 16;
+
+fn attention_gpu(
+    device: &wgpu::Device,
+    queue: &wgpu::Queue,
+    q: &[f32],
+    k: &[f32],
+    v: &[f32],
+    seq: u32,
+    d_head: u32,
+) -> Vec<f32> {
+    let buf_q = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Q"),
+        contents: bytemuck::cast_slice(&q),
+        usage: wgpu::BufferUsages::STORAGE,
+    });
+    let buf_k = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("K"),
+        contents: bytemuck::cast_slice(&k),
+        usage: wgpu::BufferUsages::STORAGE,
+    });
+    let buf_v = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("V"),
+        contents: bytemuck::cast_slice(&v),
+        usage: wgpu::BufferUsages::STORAGE,
+    });
+
+    let buf_o = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("O"),
+        size: (seq * seq * 4) as u64,
+        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
+        mapped_at_creation: false,
+    });
+
+    let dims_padded: [u32; 4] = [seq, d_head, 0, 0];
+    let buf_dims = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("dims"),
+        contents: bytemuck::cast_slice(&dims_padded),
+        usage: wgpu::BufferUsages::UNIFORM,
+    });
+
+    let module = device.create_shader_module(wgpu::include_wgsl!("../shader/attention.wgsl"));
+    let pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+        label: Some("attention"),
+        layout: None,
+        module: &module,
+        entry_point: Some("attention"),
+        compilation_options: wgpu::PipelineCompilationOptions::default(),
+        cache: None,
+    });
+
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: None,
+        layout: &pipeline.get_bind_group_layout(0),
+        entries: &[
+            wgpu::BindGroupEntry {
+                binding: 0,
+                resource: buf_q.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 1,
+                resource: buf_k.as_entire_binding(),
+            },
+            // wgpu::BindGroupEntry {
+            //     binding: 2,
+            //     resource: buf_v.as_entire_binding(),
+            // },
+            wgpu::BindGroupEntry {
+                binding: 2,
+                resource: buf_o.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 3,
+                resource: buf_dims.as_entire_binding(),
+            },
+        ],
+    });
+
+    let mut encoder =
+        device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+    {
+        let mut pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+            label: None,
+            timestamp_writes: None,
+        });
+
+        pass.set_pipeline(&pipeline);
+        pass.set_bind_group(0, &bind_group, &[]);
+
+        pass.dispatch_workgroups(seq.div_ceil(TILE), seq.div_ceil(TILE), 1);
+    }
+
+    let buf_read = device.create_buffer(&wgpu::BufferDescriptor {
+        label: None,
+        size: (seq * seq * 4) as u64,
+        usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
+        mapped_at_creation: false,
+    });
+    encoder.copy_buffer_to_buffer(&buf_o, 0, &buf_read, 0, (seq * d_head * 4) as u64);
+    queue.submit([encoder.finish()]);
+
+    let slice = buf_read.slice(..);
+    slice.map_async(wgpu::MapMode::Read, |_| {});
+
+    device.poll(wgpu::PollType::wait_indefinitely()).unwrap();
+
+    let data = slice.get_mapped_range();
+    bytemuck::allocation::pod_collect_to_vec(&data)
+}
 // CPU リファレンス
 fn attention_cpu(q: &[f32], k: &[f32], v: &[f32], seq: usize, d_head: usize) -> Vec<f32> {
     // 1 / √d_k
@@ -40,9 +151,20 @@ fn attention_cpu(q: &[f32], k: &[f32], v: &[f32], seq: usize, d_head: usize) -> 
 
 #[cfg(test)]
 mod test {
+    use super::*;
     use crate::test_utils::random_f32;
 
-    use super::*;
+    fn gpu_context() -> (wgpu::Device, wgpu::Queue) {
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor::new_without_display_handle());
+        let adapter =
+            pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions::default()))
+                .unwrap();
+        pollster::block_on(adapter.request_device(&wgpu::DeviceDescriptor {
+            required_limits: wgpu::Limits::downlevel_defaults(),
+            ..Default::default()
+        }))
+        .unwrap()
+    }
 
     #[test]
     fn test_softmax_row_sum() {
@@ -125,5 +247,67 @@ mod test {
         let out = attention_cpu(&q, &k, &v, seq, d_head);
 
         assert_eq!(out.len(), seq * d_head)
+    }
+
+    #[test]
+    fn test_softmax_row_sum_gpu() {
+        let seq: usize = 1;
+        let d_head: usize = 1;
+        let q: Vec<f32> = vec![2.0];
+        let k: Vec<f32> = vec![3.0];
+        let v: Vec<f32> = vec![4.0];
+        let (device, queue) = gpu_context();
+        let out = attention_gpu(&device, &queue, &q, &k, &v, seq as u32, d_head as u32);
+
+        // 1 / √d_k
+        // => 1.0 / √1 = 1.0
+        // QK^T / √d_k , casual mask
+        // => [2.0]*[3.0] = [6.0]
+        // score = softmax()
+        // => 1.0
+        // score * V
+        // => 1.0 * 4.0 = 4.0
+        assert!((out[0] - 6.0).abs() < 1e-4);
+    }
+
+
+    #[test]
+    fn test_casual_mask_gpu() {
+        let seq: usize = 2;
+        let d_head: usize = 2;
+        let q: Vec<f32> = vec![1.0, 0.0, 0.0, 1.0];
+        let k: Vec<f32> = vec![1.0, 1.0, 1.0, 1.0];
+        let v: Vec<f32> = vec![0.0, 1.0, 1.0, 1.0];
+        let (device, queue) = gpu_context();
+        let out = attention_gpu(&device, &queue, &q, &k, &v, seq as u32, d_head as u32);
+
+        println!("{:?}",out);
+        // q            k           v
+        // | 1.0 0.0 | | 1.0 1.0 | | 0.0 1.0 |
+        // | 0.0 1.0 | | 1.0 1.0 | | 1.0 1.0 |
+
+        // score (d_k = 2)
+        // QK^T / √d_k , casual mask
+        // | 0.7071 0      |
+        // | 0.7071 0.7071 |
+
+        // softmax
+        // seq = 0: max = 0.7071 , sum = exp(0.7071 - 0.7071) + exp(-∞ - 0.7071) = 1.0
+        // seq = 1: max = 0.7071 , sum = exp(0.7071 - 0.7071) + exp(0.7071 - 0.7071) = 2.0
+        // exp(score-max)/sum
+        // | exp(0.7071-0.7071)/1.0  exp(-∞-0.7071)/1.0     |
+        // | exp(0.7071-0.7071)/2.0  exp(0.7071-0.7071)/2.0 |
+        // =
+        // | 1.0  0.0 |
+        // | 0.5  0.5 |
+
+        // score * v
+        // | 0.0  1.0 |
+        // | 0.5  1.0 |
+        let exp: Vec<f32> = vec![0.7071, 0.7071, 0.7071, 0.7071];
+        assert!((&out[0] - &exp[0]).abs() < 1e-4);
+        assert!((&out[1] - &exp[1]).abs() < 1e-4);
+        assert!((&out[2] - &exp[2]).abs() < 1e-4);
+        assert!((&out[3] - &exp[3]).abs() < 1e-4);
     }
 }
---
+++ src/shader/attention.wgsl
@@ -0,0 +1,51 @@
+const TILE: u32 = 16u;
+
+struct Dims {
+    seq: u32,
+    d_head: u32,
+}
+
+@group(0) @binding(0) var<storage, read> Q: array<f32>;
+@group(0) @binding(1) var<storage, read> K: array<f32>;
+// @group(0) @binding(2) var<storage, read> V: array<f32>;
+@group(0) @binding(2) var<storage, read_write> O: array<f32>;
+@group(0) @binding(3) var<uniform> dims: Dims;
+
+var<workgroup> tileQ: array<array<f32, 16u>, 16u>;
+var<workgroup> tileK: array<array<f32, 16u>, 16u>;
+
+@compute @workgroup_size(TILE, TILE, 1)
+fn attention(
+    @builtin(global_invocation_id) gid: vec3<u32>,
+    @builtin(local_invocation_id) lid: vec3<u32>,
+    @builtin(workgroup_id) wid: vec3<u32>,
+) {
+    let row = gid.y;
+    let col = gid.x;
+
+    var acc: f32 = 0.0;
+    let num_tiles = (dims.d_head + TILE - 1u) / TILE;
+    // 1 / √d_k
+    let scale = 1.0 / sqrt(f32(dims.d_head));
+    // QK^T / √d_k
+    for(var t: u32 = 0u; t < num_tiles; t++) {
+        let q_col = t * TILE + lid.x;
+        let k_row = t * TILE + lid.y;
+        
+        tileQ[lid.y][lid.x] = select(0.0, Q[row * dims.d_head + q_col], row < dims.seq && q_col < dims.d_head);
+        tileK[lid.y][lid.x] = select(0.0, K[col * dims.d_head + k_row], k_row < dims.d_head && col < dims.seq);
+
+        workgroupBarrier();
+
+        for(var i: u32 = 0u; i < TILE; i++) {
+            acc += tileQ[lid.y][i] * tileK[lid.x][i];
+        }
+
+        workgroupBarrier();
+   
+    }
+
+    if row < dims.seq && col < dims.d_head {
+        O[row * dims.seq + col] = acc * scale;
+    }    
+}
